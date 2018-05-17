ef244eadac0f6d500c5104b9cc8901627ccd30e7
Ram Kumar <ram.kumar@gaussnetworks.com>
2018-05-17 20:21:22 +0530

    Initial check-in of module Mobiskribe

---
+++ Mobiskribe/Utils/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Mobiskribe.Utils
+{
+    public class Constants
+    {
+        public Constants()
+        {
+        }
+
+        public class StoryBoardFile
+        {
+            public const string LOGIN_VIEW = "LoginView";
+        }
+
+        public class StoryBoardID
+        {
+            public const string LOGINVIEWCONTROLLER = "LoginViewController";
+        }
+    }
+}
